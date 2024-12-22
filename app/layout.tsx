9d40e54ac895f1a13710b2093c6f7fc21c987b6d
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-12-22 18:45:38 -0300

    chore: tailwind e módulos CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+// importação do CSS global
+import "@/app/ui/global.css"
+
+/**
+ * Componente de nível superior da aplicação.
+ * @author Next.js
+ */
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
