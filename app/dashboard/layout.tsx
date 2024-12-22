a01284ea38e97ca52397b9730e33de33757c2dbc
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-12-22 19:48:21 -0300

    feat: criação do layout de dashboard

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,23 @@
+import SideNav from "@/app/ui/dashboard/sidenav"
+
+/** Interface dos props de `Layout`. */
+interface LayoutProps {
+  /** Filho do componente */
+  children: React.ReactNode
+}
+/** Layout da página de dashboard. */
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      {/* Sidebar */}
+      <div className="w-full flex-none md:w-64"></div>
+      <SideNav />
+
+      {/* Main content */}
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+        {/* Filhos */}
+        {children}
+      </div>
+    </div>
+  )
+}
