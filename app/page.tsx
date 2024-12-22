046f8c788db6b697e7c26f30566d4a9e4b27e1ea
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-12-22 19:26:07 -0300

    feat: otimização de imagens

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,17 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline"
 import Link from "next/link"
 // importação da fonte Lusitana
 import { lusitana } from "@/app/ui/fonts"
-
-// módulos CSS
-import styles from "@/app/ui/home.module.css"
+// importação do componente que otimiza imagens
+import Image from "next/image"
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      {/* Exemplo de módulo CSS */}
-      <div className={styles.shape}></div>
-
-      {/* Logo */}
-      <AcmeLogo />
-
       {/* Conteúdo */}
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">{/* <AcmeLogo /> */}</div>
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+        {/* Logo */}
+        <AcmeLogo />
+      </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           {/* Texto de boas-vindas com fonte Lusitana. */}
@@ -32,7 +28,20 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">{/* Add Hero Images Here */}</div>
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          {/* Hero Image Desktop */}
+          <Image
+            src={"/hero-desktop.png"} // URL da imagem (nas pasta `/public`)
+            alt="Hero Image" // Texto alternativo
+            // Tamanho da imagem
+            width={1000}
+            height={760}
+            // classes CSS
+            className="hidden md:block" // visível apenas em telas grandes
+          />
+          {/* Hero Image Mobile */}
+          <Image src={"/hero-mobile.png"} alt="Hero Image" width={560} height={620} className="block md:hidden" />
+        </div>
       </div>
     </main>
   )
