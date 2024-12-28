22ab0ce5d15aaf4cf24c65d4e64bc3aac4b8186f
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-12-27 21:12:25 -0300

    feat: skeleton da página de Dashboard

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+import DashboardSkeleton from "@/app/ui/skeletons"
+
+/** Componente exibido enquanto conteúdo da página de Dashboard ainda está sendo carregado. */
+export default function Loading() {
+  return <DashboardSkeleton />
+}
