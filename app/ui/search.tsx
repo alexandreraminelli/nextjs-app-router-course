0aae8648f26ce6b80b7f5f93cf939f52b7e8689d
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-12-28 19:16:50 -0300

    feat: função que captura os termos de pesquisa

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,10 +2,19 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline" // ícone de pesquisa
 
-/** Componente da caixa de pesquisa. */
+/** Componente da caixa de pesquisa.
+ * @author Next.js
+ */
 export default function Search({ placeholder }: { placeholder: string }) {
-  /**  */
-  
+  /**
+   * Função que
+   * @param term Termo da pesquisa
+   *
+   * @author Alexandre Raminelli
+   */
+  function handleSearch(term: string) {
+    console.log(term)
+  }
 
   // retorno do componente
   return (
@@ -19,6 +28,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500" // classes CSS
         placeholder={placeholder} // texto de placeholder
+        // Quando ocorrer uma mudança no elemento:
+        onChange={(e) => {
+          handleSearch(e.target.value) // passar texto do input como parâmetro na função `handleSearch`
+        }}
       />
 
       {/* Ícone de pesquisa */}
